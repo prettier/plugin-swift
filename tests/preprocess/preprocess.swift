c2f5b9941975438104d9afc6a9378c79deeac4d7
Marcel Jackwerth <marceljackwerth@gmail.com>
2018-01-07 08:06:19 +0100

    Add regression test for prettier preprocessing

---
+++ tests/preprocess/preprocess.swift
@@ -0,0 +1,26 @@
+// All these semicolons need to be stripped.
+// If prettier is not aware of the preprocessing,
+// it will create a blank line between b() and c() further below.
+x;
+x;
+x;
+x;
+x;
+x;
+x;
+x;
+x;
+x;
+x;
+x;
+x;
+x;
+x;
+x;
+x;
+x;
+x;
+
+a()
+    .b()
+    .c()
