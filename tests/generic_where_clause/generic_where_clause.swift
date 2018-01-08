04b97902209f8c8565566e3f279b8546de452541
Marcel Jackwerth <marceljackwerth@gmail.com>
2018-01-08 09:19:41 +0100

    More tests for checking break in front of `where`

--- tests/generic_where_clause/generic_where_clause.swift
+++ tests/generic_where_clause/generic_where_clause.swift
@@ -5,3 +5,13 @@ extension ShapedFruit where Shape: Pear {
 extension TheQuickBrownFoxJumpsOverTheLazyDogTheQuickBrownFoxJumpsOverTheLazyDog where TheLazyDog: VeryLazyDog {
 
 }
+
+class Fox {
+    func jump<Dog>(over: Dog) where Dog: LazyDog {
+
+    }
+
+    func theQuickBrownFoxJumpsOverTheLazyDogTheQuickBrownFoxJumpsOver<TheLazyDog>() where TheLazyDog: VeryLazyDog {
+
+    }
+}
