3732476520da5ebcb66bc45ab8fb53a4e8b008f9
Marcel Jackwerth <marceljackwerth@gmail.com>
2018-01-10 22:57:36 +0100

    Add tests for breaking 'try'

--- tests/try/try.swift
+++ tests/try/try.swift
@@ -1,7 +1,15 @@
 try somethingDangerous()
 
 _ = try? somethingDangerous()
+
 let x = try? somethingDangerous()
 
 try! somethingDangerous()
+
 let x = try! somethingDangerous()
+
+let theQuickBrownFoxJumpsOverTheLazyDog = try makeTheQuickBrownFoxJumpsOverTheLazyDog(fox: fox, dog: dog)
+
+let theQuickBrownFoxJumpsOverTheLazyDog = try? makeTheQuickBrownFoxJumpsOverTheLazyDog(fox: fox, dog: dog)
+
+let theQuickBrownFoxJumpsOverTheLazyDog = try! makeTheQuickBrownFoxJumpsOverTheLazyDog(fox: fox, dog: dog)
